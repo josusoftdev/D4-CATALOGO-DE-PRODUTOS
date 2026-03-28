76a5285142564fc8a40c2034f162f082ce8e144b
Josué Olbertz Farah de Castilhos <160082875+josusoftdev@users.noreply.github.com>
2026-03-27 22:15:33 -0300

    Create page.tsx

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage(){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <p>Bem-vindo ao dashboard!</p>
+            <p>Aqui você pode visualizar suas informações principais.</p>
+        </div>
+    );
+}
