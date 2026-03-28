b9b384cb90fcf0981822465eb09212fb9ddfc917
Josué Olbertz Farah de Castilhos <160082875+josusoftdev@users.noreply.github.com>
2026-03-27 22:17:45 -0300

    Create page.tsx

---
+++ app/(dashboard)/products/[id]/page.tsx
@@ -0,0 +1,15 @@
+// app/product/[id]/BackButton.tsx
+
+"use client"
+
+import { useRouter } from 'next/navigation'
+
+export default function BackButton() {
+  const router = useRouter()
+
+  return (
+    <button onClick={() => router.back()}>
+      Voltar
+    </button>
+  )
+}
